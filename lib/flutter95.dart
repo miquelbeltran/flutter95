2b57b0fd9f301597f7aaed1befb1d6f1503a9d86
lukepighetti <lukepighetti@gmail.com>
2021-04-27 21:14:55 -0400

    add pageTransitions95

--- lib/flutter95.dart
+++ lib/flutter95.dart
@@ -8,4 +8,5 @@ export 'src/menu95.dart';
 export 'src/scaffold95.dart';
 export 'src/text_field95.dart';
 export 'src/toolbar95.dart';
+export 'src/page_transitions95.dart';
 export 'src/utils.dart';
