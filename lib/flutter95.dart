c263b424b8190b56782ab5e9f978343b48bdce56
Taosif7 <taosif.7@gmail.com>
2023-10-21 20:29:47 +0530

    refactor: Changed filename as per project convention

--- lib/flutter95.dart
+++ lib/flutter95.dart
@@ -7,7 +7,7 @@ export 'src/globals.dart';
 export 'src/menu95.dart';
 export 'src/scaffold95.dart';
 export 'src/text_field95.dart';
-export 'src/checkbox_95.dart';
+export 'src/checkbox95.dart';
 export 'src/toolbar95.dart';
 export 'src/page_transitions95.dart';
 export 'src/utils.dart';
